abcd3e490fe5bc59d6b014ba49d63696513e069b
Shubham Tiwari <tshubh29@gmail.com>
2025-08-02 10:56:04 +0530

    Built User service and Activity service

---
+++ activity-service/src/main/java/com/fitness/activityservice/modal/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.modal;
+
+public enum ActivityType {
+    RUNNING,
+    WEIGHT_TRAINING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    YOGA,
+    HIIT,
+    STRECHING,
+    OTHER
+}
