decb2718465054273e01abacf477c2213273e1b9
Shubham Tiwari <tshubh29@gmail.com>
2025-08-03 11:34:45 +0530

    RabbitMQ and Web Client configuration added

--- activity-service/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activity-service/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -30,7 +30,6 @@ public class ActivityController {
         return ResponseEntity.ok(activityService.getUserActivities(userId));
     }
 
-
     @GetMapping("/{activityId}")
     public ResponseEntity<ActivityResponse> getActivity(@PathVariable String activityId){
         return ResponseEntity.ok(activityService.getActivityById(activityId));
